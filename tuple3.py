d9a8b2f4f861e1cbd0cb2a769ba1b0b0396778dc
lohithacseds <slohitha465@gmail.com>
2024-10-03 01:37:21 -0700

    Create tuple3.py

---
+++ tuple3.py
@@ -0,0 +1,9 @@
+//
+ Write a program to get the tuple elements in a single line separated by spaces and print the number of elements in the given tuple.
+Sample Input:
+10 20 30
+Sample Output:
+3
+//
+my_tuple = tuple(map(int, input().split()))
+print(len(my_tuple))   
